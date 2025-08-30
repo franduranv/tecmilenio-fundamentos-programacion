dbc08079f83d9dac502b2eada5bba3b00c78c5ac
Fran Durán <franduranv@gmail.com>
2025-08-30 13:50:07 -0600

    Agrego ejercicios de programación en Python: verificación de paridad, clasificación de triángulos, conteo hasta n, validación de usuario y suma de números.

---
+++ Python_Sesiones/Sesión 05/02.py
@@ -0,0 +1,8 @@
+#Cuenta números
+#Escribe un programa que lea un número positivo n e imprima todos los números en ordesn desde 1 hasta n. Cada uno de los numeros debe ser impreso.
+
+n = int(input("Ingrese un número positivo: "))
+for i in range(1,n+1):
+    print(i)
+    
+#for i in range(1, n+1):  # Correcto
